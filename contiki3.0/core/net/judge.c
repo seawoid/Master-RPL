d92f967c7b9951bd2318e0dc9b90d07fe37515f3
seawoid <bigseaweed1227@gmail.com>
2021-10-12 20:50:00 +0800

    first commit

---
+++ contiki3.0/core/net/judge.c
@@ -0,0 +1,5 @@
+#include "judge.h"
+int topo_num = 60L;
+int E  = 20L;
+int Po = 40L;
+int R  = 40L;
---
+++ contiki3.0/core/net/judge.h
@@ -0,0 +1,12 @@
+#ifndef JUDGE_H_
+#define JUDGE_H_
+
+#define battery 7500L //7500m
+
+//#define ETX_max 2560
+//#define Rank_max 10
+extern int E, Po, R, topo_num;
+
+#endif
+
+
